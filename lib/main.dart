f5f236356b007ae1b538450273d0629f74207aa6
mohammed <mohammedtamernasar7779@gmail.com>
2026-01-06 21:01:24 +0200

    Refactor of provide of cubit

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:notesapp1/constants.dart';
-import 'package:notesapp1/cubits/add_note_cubit/add_note_cubit.dart';
 import 'package:notesapp1/models/note_model.dart';
 import 'package:notesapp1/simple_bloc_observer.dart';
 import 'package:notesapp1/views/Notes_view.dart';
@@ -20,13 +19,10 @@ class NotesApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MultiBlocProvider(
-      providers: [BlocProvider(create: (context) => AddNoteCubit())],
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(brightness: Brightness.dark, fontFamily: 'Poppins'),
-        home: NotesView(),
-      ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(brightness: Brightness.dark, fontFamily: 'Poppins'),
+      home: NotesView(),
     );
   }
 }
